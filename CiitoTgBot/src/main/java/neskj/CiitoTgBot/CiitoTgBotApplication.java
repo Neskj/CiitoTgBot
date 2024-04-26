9d3fedbbab889eabf81171197c9dbe86ba8170f5
Neskj <pronin.qq@gmail.com>
2024-04-26 12:19:43 +0300

    add start files tg boot

---
+++ CiitoTgBot/src/main/java/neskj/CiitoTgBot/CiitoTgBotApplication.java
@@ -0,0 +1,13 @@
+package neskj.CiitoTgBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiitoTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiitoTgBotApplication.class, args);
+	}
+
+}
---
+++ CiitoTgBot/src/test/java/neskj/CiitoTgBot/CiitoTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package neskj.CiitoTgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiitoTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
