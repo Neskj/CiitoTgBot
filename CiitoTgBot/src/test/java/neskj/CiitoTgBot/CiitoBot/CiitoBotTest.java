0551ca1ddcf2b9c45bd81c08cf8bd1519a31cba5
Neskj <pronin.qq@gmail.com>
2024-05-03 13:03:10 +0300

    commit work process

--- CiitoTgBot/src/test/java/neskj/CiitoTgBot/CiitoBot/CiitoBotTest.java
+++ CiitoTgBot/src/test/java/neskj/CiitoTgBot/CiitoBot/CiitoBotTest.java
@@ -6,8 +6,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CiitoBotTest {
@@ -23,12 +29,34 @@ public class CiitoBotTest {
 
         assertEquals("testBotName",bot.getBotUsername());
     }
-    
+
     @Test
     public void getBotTokenHappyFlow(){
 
         assertEquals("7463523662:FSBJFgTgRbR5GSRGDh42fges63g0w5CtDEj",bot.getBotToken());
     }
+/*
+    @Test
+    public void onUpdateReceivedTestHappyFlow(){
+
+        Long testChatId=123456789L;
+        String text="/start";
+
+        Chat testChat=new Chat();
+        testChat.setId(testChatId);
+
+        Message testMessage=new Message();
+        testMessage.setChat(testChat);
+        testMessage.setText(text);
+
+        Update testUpdate=new Update();
+        testUpdate.setMessage(testMessage);
+
+        when(response.getResponse(text)).thenReturn("Привет! Я новый бот Цеха испытаний и техобслуживания! Чем могу помочь ?");
+
+        verify(response).getResponse("Привет! Я новый бот Цеха испытаний и техобслуживания! Чем могу помочь ?");
 
 
+    }
+*/
 }
